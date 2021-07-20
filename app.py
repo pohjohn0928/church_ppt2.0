64b3ccf2d4455f9754224e0b0b385bd38be7e074
John Poh <johnpoh@JohndeMacBook-Pro.local>
2021-07-20 14:55:18 +0800

    GCP
    add requirements.txt
    add host and port = 80

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def getPdfFile():
         return f"PPT Path : {os.path.dirname(__file__)}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = '0.0.0.0',port = 80,debug=False)
